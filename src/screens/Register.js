b46e45aea99fb63955e1add64069fb170302fce7
SyxHieu <133748573+SyxHieu@users.noreply.github.com>
2023-06-28 18:19:16 +0700

    update

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,206 +1,153 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
-
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import { Link } from "react-router-dom";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+const Register = () => {
+
+    const [account, accountchange] = useState("");
+    const [name, namechange] = useState("");
+    const [password, passwordchange] = useState("");
+    const [gmail, gmailchange] = useState("");
+    const [phone, phonechange] = useState("");
+    const [country, countrychange] = useState("india");
+    const [address, addresschange] = useState("");
+    const [gender, genderchange] = useState("female");
+
+    const navigate = useNavigate();
+
+    const IsValaccountate = () => {
+        let isproceed = true;
+        let errormessage = 'Please enter the value in ';
+        if (account === null || account === '') {
+            isproceed = false;
+            errormessage += ' Username';
+        }
+        if (name === null || name === '') {
+            isproceed = false;
+            errormessage += ' Fullname';
+        }
+        if (password === null || password === '') {
+            isproceed = false;
+            errormessage += ' Password';
+        }
+        if (gmail === null || gmail === '') {
+            isproceed = false;
+            errormessage += ' gmail';
+        }
+
+        if(!isproceed){
+            toast.warning(errormessage)
+        }else{
+            if(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(gmail)){
+
+            }else{
+                isproceed = false;
+                toast.warning('Please enter the valaccount gmail')
+            }
+        }
+        return isproceed;
+    }
 
 
-export default function Register() {
-    const [user, setUser] = useState("");
-    const [password, setPassword] = useState("");
-    const [fname, setFname] = useState("");
-    const [email, setEmail] = useState("");
-    const [phone, setPhone] = useState("");
-    const [gender, setGender] = useState("");
-    const [city, setCity] = useState("");
-    const [address, setAddress] = useState("");
-    const nav = useNavigate();
-
-    const handleLogin = () => {
-        if (user && password && fname && address && city&& phone  && email&& gender) {
-            alert("Register Successfull");
-            nav('/login');
+    const handlesubmit = (e) => {
+            e.preventDefault();
+            let regobj = { account, name, password, gmail, phone, country, address, gender };
+            if (IsValaccountate()) {
+            //console.log(regobj);
+            fetch("http://localhost:9999/users", {
+                method: "POST",
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify(regobj)
+            }).then((res) => {
+                toast.success('Registered successfully.')
+                navigate('/login');
+            }).catch((err) => {
+                toast.error('Failed :' + err.message);
+            });
         }
     }
     return (
+        <div>
+            <div className="offset-lg-3 col-lg-6">
+                <form className="container" onSubmit={handlesubmit}>
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>User Registeration</h1>
+                        </div>
+                        <div className="card-body">
 
-        <Container class="container">
-            <Row className="vh-100 d-flex justify-content-center align-items-center">
-                <Col md={8} lg={6} xs={12}>
-                    <div className="border border-3 border-primary"></div>
-                    <Card className="shadow">
+                            <div className="row">
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>User Name <span className="errmsg">*</span></label>
+                                        <input value={account} onChange={e => accountchange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Password <span className="errmsg">*</span></label>
+                                        <input value={password} onChange={e => passwordchange(e.target.value)} type="password" className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Full Name <span className="errmsg">*</span></label>
+                                        <input value={name} onChange={e => namechange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Gmail <span className="errmsg">*</span></label>
+                                        <input value={gmail} onChange={e => gmailchange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Phone <span className="errmsg"></span></label>
+                                        <input value={phone} onChange={e => phonechange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Country <span className="errmsg">*</span></label>
+                                        <select value={country} onChange={e => countrychange(e.target.value)} className="form-control">
+                                            <option value="vietnam">VietNam</option>
+                                            <option value="usa">USA</option>
+                                            <option value="singapore">Singapore</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col-lg-12">
+                                    <div className="form-group">
+                                        <label>Address</label>
+                                        <textarea value={address} onChange={e => addresschange(e.target.value)} className="form-control"></textarea>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Gender</label>
+                                        <br></br>
+                                        <input type="radio" checked={gender === 'male'} onChange={e => genderchange(e.target.value)} name="gender" value="male" className="app-check"></input>
+                                        <label>Male</label>
+                                        <input type="radio" checked={gender === 'female'} onChange={e => genderchange(e.target.value)} name="gender" value="female" className="app-check"></input>
+                                        <label>Female</label>
+                                    </div>
+                                </div>
 
-                        <Card.Header style={{ background: "#eee" }}>
-                            <Card.Title>User Registration</Card.Title>
-                        </Card.Header>
+                            </div>
 
-                        <Card.Body>
-
-                            <Form>
-                                <Row>
-                                    <Col>
-                                        <Form.Group
-                                            className="mb-3"
-                                            controlId="formGroupUsername"
-                                        >
-                                            <Form.Label>
-                                                Username
-                                                <span style={{ color: "red" }}>
-                                                    *
-                                                </span>
-                                            </Form.Label>
-                                            <Form.Control type="text" onChange={(e) => setUser(e.target.value)} />
-
-                                        </Form.Group>
-                                        <p style={{ color: 'red' }}>{user ? "" : "Please enter username"}</p>
-                                    </Col>
-                                    <Col>
-                                        <Form.Group
-                                            className="mb-3"
-                                            controlId="formGroupPassword"
-                                        >
-                                            <Form.Label>
-                                                Password
-                                                <span style={{ color: "red" }}>
-                                                    *
-                                                </span>
-                                            </Form.Label>
-                                            <Form.Control type="password" onChange={(e) => setPassword(e.target.value)} />
-
-                                        </Form.Group>
-                                        <p style={{ color: 'red' }}>{password ? "" : "Please enter password"}</p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col>
-                                        <Form.Group
-                                            className="mb-3"
-                                            controlId="formGroupFullName"
-                                        >
-                                            <Form.Label>
-                                                Fullname
-                                                <span style={{ color: "red" }}>
-                                                    *
-                                                </span>
-                                            </Form.Label>
-                                            <Form.Control type="text" onChange={(e) => setFname(e.target.value)} />
-                                        </Form.Group>
-                                        <p style={{ color: 'red' }}>{fname ? "" : "Please enter Fullname"}</p>
-                                    </Col>
-                                    <Col>
-                                        <Form.Group
-                                            className="mb-3"
-                                            controlId="formGroupEmail"
-                                        >
-                                            <Form.Label for="staticEmail">
-                                                Email
-                                                <span style={{ color: "red" }}>
-                                                    *
-                                                </span>
-                                            </Form.Label>
-                                            <Form.Control type="text" onChange={(e) => setEmail(e.target.value)} readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"></Form.Control> 
-                                        </Form.Group>
-                                        <p style={{ color: 'red' }}>{fname ? "" : "Please enter Fullname"}</p>
-                                    </Col>
-                                    
-                                </Row>
-                                <Row>
-                                    <Col>
-                                        <Form.Group
-                                            className="mb-3"
-                                            controlId="formGroupPhone"
-                                        >
-                                            <Form.Label>
-                                                Phone
-                                                <span style={{ color: "red" }}>
-                                                    *
-                                                </span>
-                                            </Form.Label>
-                                            <Form.Control type="text" onChange={(e) => setPhone(e.target.value)} />
-                                        </Form.Group>
-                                        <p style={{ color: 'red' }}>{phone ? "" : "Please enter NumberPhone"}</p>
-                                    </Col>
-                                    <Col>
-                                        <Form.Group
-                                            className="mb-3"
-                                            controlId="formGroupCountry"
-                                        >
-                                            <Form.Label>
-                                                City
-                                                <span style={{ color: "red" }}>
-                                                    *
-                                                </span>
-                                            </Form.Label>
-                                            <Form.Control type="text" onChange={(e) => setCity(e.target.value)} />
-                                      
-                                        </Form.Group>
-                                        <p style={{ color: 'red' }}>{city ? "" : "Please choose City"}</p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col>
-                                        <Form.Group
-                                            className="mb-3"
-                                            controlId="formGroupAddress"
-                                        >
-                                            <Form.Label>Address</Form.Label>
-                                            <Form.Control type="text" onChange={(e) => setAddress(e.target.value)} />
-                                        </Form.Group>
-                                        <p style={{ color: 'red' }}>{address ? "" : "Please enter Address"}</p>
-                                    </Col>
-                                    <Col>
-                                        <Form.Group
-                                            className="mb-3"
-                                            controlId="formGroupCountry"
-                                        >
-                                            <Form.Label>
-                                                Gender
-                                                <span style={{ color: "red" }}>
-                                                    *
-                                                </span>
-                                            </Form.Label>
-                                            <Form.Select required  onChange={(e) => setGender(e.target.value)}>
-                                                <option value="0">
-                                                    ---Choose---
-                                                </option>
-                                                <option value="0">Nam</option>
-                                                <option value="1">Nữ</option>
-                                               
-                                            </Form.Select>
-                                        </Form.Group>
-                                        <p style={{ color: 'red' }}>{gender ? "" : "Please choose gender:"}</p>
-                                    </Col>
-                                </Row>
-                            </Form>
-                        </Card.Body>
-
-                        <Card.Footer
-                            style={{
-                                background: "#eee",
-                                justifyContent: "flex-start",
-                            }}
-                        >
-                            <Button onClick={handleLogin}>
-                          Register
-                        </Button>
-                            <Link to="/">
-                                <Button
-                                    style={{ background: "red" }}
-                                    variant="primary"
-                                >
-                                    Back
-                                </Button>
-                            </Link>
-                        </Card.Footer>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+                        </div>
+                        <div className="card-footer">
+                            <button type="submit" className="btn btn-primary">Register</button> |
+                            <Link to={'/login'} className="btn btn-danger">Close</Link>
+                        </div>
+                    </div>
+                </form>
+            </div>
 
 
-
+        </div>
     );
 }
+
+export default Register;
