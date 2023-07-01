02cf0d046434aab28a2dc3814041f47123b9e421
NguyenVanThang <thangnvhe161412@fpt.edu.vn>
2023-07-01 21:28:21 +0700

    implementtags

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ThreadListScreen from './screens/ThreadListScreen';  
 import ThreadDetailScreen from './screens/ThreadDetailScreen';
+import CreateTags from './components/CreateTags';
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <BrowserRouter>
           <Routes>
               <Route path="/" element={<ThreadListScreen />} />
-              <Route path="/thread/:threadId" element={<ThreadDetailScreen />} />
+              <Route path="/" element={<CreateTags />} />
+              
           </Routes>
       </BrowserRouter>
   );
