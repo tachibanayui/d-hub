f154a4d7c0f73d6bedc2a8093f4b40000223c7d0
tachibanayui <quan.tachibanayui@outlook.com>
2023-07-17 00:44:46 +0700

    Fix broken links

--- src/components/TagBadge.jsx
+++ src/components/TagBadge.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const TagBadge = ({tag}) => {
     return (
         <Badge bg="secondary" className="m-1">
-            <Link key={tag.id} to={`/tags`} title={tag.description}>
+            <Link key={tag.id} to={`/tag`} title={tag.description}>
                 <span style={{color: 'white'}}>{tag.topic}</span>
             </Link>
         </Badge>
